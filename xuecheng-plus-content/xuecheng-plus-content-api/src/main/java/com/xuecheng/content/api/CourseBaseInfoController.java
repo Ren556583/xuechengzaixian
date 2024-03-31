f1bcf9aa8c08eb124f59064f771c0d2764a3e8d8
renyuhang <906656563@qq.com>
2024-03-31 21:29:32 +0800

    课程查询，课程分类查询，新增课程等

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -2,27 +2,18 @@ package com.xuecheng.content.api;
 
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
-<<<<<<< HEAD
 import com.xuecheng.content.model.dto.AddCourseDto;
 import com.xuecheng.content.model.dto.CourseBaseInfoDto;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
 import com.xuecheng.content.service.CourseBaseInfoService;
-=======
-import com.xuecheng.content.model.dto.QueryCourseParamsDto;
-import com.xuecheng.content.model.po.CourseBase;
->>>>>>> 6f86c248a53265cef93ff0d4afa5bc1d25aa45ce
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD
 import javax.annotation.Resource;
-=======
->>>>>>> 6f86c248a53265cef93ff0d4afa5bc1d25aa45ce
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,11 +24,10 @@ import java.util.List;
  * @description TODO
  * @date 2023/2/11 15:44
  */
-@Api(value = "课程信息管理接口",tags = "课程信息管理接口")
+@Api(value = "课程信息管理接口", tags = "课程信息管理接口")
 @RestController
 public class CourseBaseInfoController {
 
-<<<<<<< HEAD
     @Resource
     private CourseBaseInfoService courseBaseInfoService;
 
@@ -55,20 +45,5 @@ public class CourseBaseInfoController {
         // 机构id，暂时硬编码模拟假数据
         Long companyId = 22L;
         return courseBaseInfoService.createCourseBase(companyId, addCourseDto);
-=======
-    @ApiOperation("课程查询接口")
-    @PostMapping("/course/list")
-    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required=false) QueryCourseParamsDto queryCourseParamsDto) {
-
-
-        CourseBase courseBase = new CourseBase();
-        courseBase.setName("测试名称");
-        courseBase.setCreateDate(LocalDateTime.now());
-        List<CourseBase> courseBases = new ArrayList();
-        courseBases.add(courseBase);
-        PageResult pageResult = new PageResult<CourseBase>(courseBases,10,1,10);
-        return pageResult;
-
->>>>>>> 6f86c248a53265cef93ff0d4afa5bc1d25aa45ce
     }
 }
