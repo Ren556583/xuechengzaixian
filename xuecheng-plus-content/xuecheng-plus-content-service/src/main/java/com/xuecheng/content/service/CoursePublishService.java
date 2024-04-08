14e81b86e335254f89cbfafb453802797f1d3960
renyuhang <906656563@qq.com>
2024-04-08 15:31:24 +0800

    课程预览，课程审核

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CoursePublishService.java
@@ -0,0 +1,22 @@
+package com.xuecheng.content.service;
+
+
+import com.xuecheng.content.model.dto.CoursePreviewDto;
+
+/**
+ * 课程预览、发布接口
+ */
+public interface CoursePublishService {
+    /**
+     * 根据课程id获取课程预览信息
+     * @param courseId  课程id
+     * @return  package com.xuecheng.content.model.dto.CoursePreviewDto;
+     */
+    CoursePreviewDto getCoursePreviewInfo(Long courseId);
+
+    /**
+     * 提交审核
+     * @param courseId  课程id
+     */
+    void commitAudit(Long companyId, Long courseId);
+}
